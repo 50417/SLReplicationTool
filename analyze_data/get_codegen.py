0fdf15b608dca6d9120dbe9042977c13d7c7b5be
Anonymous-double-blind <anonymous299424@gmail.com>
2023-05-01 17:52:48 -0500

    Added support for code gen models

--- analyze_data/get_codegen.py
+++ analyze_data/get_codegen.py
@@ -92,6 +92,41 @@ def get_code_generating_models_project(conn,table, where_cond = None):
     #print("Total: {}".format(total ))
 
     return str(embedded_boll)+" & "+str(embedded)+" & "+str(grt)+" & "+str(others)+" & "+str(total)
+
+def get_code_generating_models(conn,table, where_cond = None):
+    embedded_boll_sql = "select count(*) from "+table+"_code_gen where Embeddedcoder = 1 "
+    if where_cond is not None:
+        embedded_boll_sql += where_cond
+    embedded_boll = get_all_vals_from_table(conn,embedded_boll_sql)
+
+    target_link_boll_sql = "select count(*) from "+table+"_code_gen where TargetLink = 1 "
+    if where_cond is not None:
+        target_link_boll_sql += where_cond
+    target_link_boll = get_all_vals_from_table(conn,target_link_boll_sql)
+
+
+    embedded_sql = "select count(*) from "+table+"_code_gen where System_Target_File in  ('ert.tlc','ert_shrlib.tlc') and Solver_Type =='Fixed-step' "
+    if where_cond is not None:
+        embedded_sql += where_cond
+    embedded = get_all_vals_from_table(conn,embedded_sql)
+
+    grt_sql = "select count(*) from "+table+"_code_gen where System_Target_File in  ('grt.tlc') and Solver_Type =='Fixed-step' "
+    if where_cond is not None:
+        grt_sql += where_cond
+    grt = get_all_vals_from_table(conn,grt_sql)
+    
+    others_sql = ' select count(*) from '+table+'_code_gen where System_Target_File not in  ("grt.tlc","ert.tlc","ert_shrlib.tlc") and (System_Target_File in ("rsim.tlc","rtwsun.tlc")  or Solver_Type =="Fixed-step") '
+    if where_cond is not None:
+        others_sql += where_cond
+    others = get_all_vals_from_table(conn,others_sql)
+
+    total_sql = '  select count(*) from '+table+'_code_gen where (System_Target_File in  ("rsim.tlc","rtwsun.tlc")  or ( System_Target_File not in  ("rsim.tlc","rtwsun.tlc") and Solver_Type =="Fixed-step")) '
+    if where_cond is not None:
+        total_sql += where_cond
+    total = get_all_vals_from_table(conn,total_sql)
+
+
+    return str(embedded_boll)+" & "+str(embedded)+" & "+str(grt)+" & "+str(others)+" & "+str(total)
     
 
 def main():
@@ -102,10 +137,17 @@ def main():
     # create a database connection
     conn = create_connection(slc_20r_2020b_database)
     for i in range(len(category)): 
+        #project_sql = "Select ID from "+table+"_projects where category = '"+ category[i]+"'"
+        #no_of_projects, id_sql_when_cond = get_project_ids_from_table(conn,project_sql)
+        #where_cond = "AND FILE_ID IN ("+id_sql_when_cond+")"
+        #print(row_title[i]+" & "+str(no_of_projects)+" & "+get_code_generating_models_project(conn,table,where_cond)+"\\\\")
+        total_models_sql = "Select count(*) from "+table+"_Models WHERE FILE_ID in  (SELECT ID FROM "+table+"_projects where category = '"+ category[i]+"') AND is_lib = 0 and is_test=-1"
+        total_models = get_all_vals_from_table(conn,total_models_sql)
         project_sql = "Select ID from "+table+"_projects where category = '"+ category[i]+"'"
         no_of_projects, id_sql_when_cond = get_project_ids_from_table(conn,project_sql)
         where_cond = "AND FILE_ID IN ("+id_sql_when_cond+")"
-        print(row_title[i]+" & "+str(no_of_projects)+" & "+get_code_generating_models_project(conn,table,where_cond)+"\\\\")
+        print(row_title[i]+" & "+str(total_models)+" & "+get_code_generating_models(conn,table,where_cond)+"\\\\")
+    
     print("=================================================")
 
 
@@ -123,9 +165,13 @@ def main():
     conn = create_connection(slc_r_2020b_database)
 
     for table in tables:
-        total_project_id_sql = "Select count(distinct FILE_ID) from "+table+"_models"
-        total_project_id = get_all_vals_from_table(conn,total_project_id_sql)
-        print(table+" & "+str(total_project_id)+" & "+get_code_generating_models_project(conn,table,extra_where_cond)+"\\\\")
+        #total_project_id_sql = "Select count(distinct FILE_ID) from "+table+"_models"
+        #total_project_id = get_all_vals_from_table(conn,total_project_id_sql)
+        #print(table+" & "+str(total_project_id)+" & "+get_code_generating_models_project(conn,table,extra_where_cond)+"\\\\")
+        total_models_sql = "Select count(*) from "+table+"_models WHERE is_lib = 0 and is_test=-1"+ extra_where_cond
+        total_models = get_all_vals_from_table(conn,total_models_sql)
+        print(table+" & "+str(total_models)+" & "+get_code_generating_models(conn,table,extra_where_cond)+"\\\\")
+    
     print("=================================================")
 
 
@@ -134,10 +180,12 @@ def main():
     # create a database connection
     conn = create_connection(slnet_2020b_database)
     for table in tables:
-        total_project_id_sql = "Select count(distinct FILE_ID) from "+table+"_models"
-        total_project_id = get_all_vals_from_table(conn,total_project_id_sql)
-        print(table+" & "+str(total_project_id)+" & "+get_code_generating_models_project(conn,table)+"\\\\")
-
+        #total_project_id_sql = "Select count(distinct FILE_ID) from "+table+"_models"
+        #total_project_id = get_all_vals_from_table(conn,total_project_id_sql)
+        #print(table+" & "+str(total_project_id)+" & "+get_code_generating_models_project(conn,table)+"\\\\")
+        total_models_sql = "Select count(*) from "+table+"_models WHERE is_lib = 0 and is_test=-1"
+        total_models = get_all_vals_from_table(conn,total_models_sql)
+        print(table+" & "+str(total_models)+" & "+get_code_generating_models(conn,table)+"\\\\")
 
 
 
